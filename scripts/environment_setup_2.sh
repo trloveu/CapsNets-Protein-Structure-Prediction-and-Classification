170d66addba7ef9822486615d80d40dc98d212cd
Chameleon Cloud User <cc@tensoflow-gpu-support.novalocal>
2018-07-18 00:20:08 +0000

    updates to script

--- scripts/environment_setup_2.sh
+++ scripts/environment_setup_2.sh
@@ -1,5 +1,23 @@
 cd /tmp
 
 echo "********** CREATING CONDA ENVIRONMENT **********"
-conda create --name volumetric_cpu -f cpu.yml
-source activate volumetric_cpu
+conda create -n volumetric python=3.6 -y
+source activate volumetric
+
+pip install scipy matplotlib pillow imutils h5py requests progressbar2 scikit-learn scikit-image wget numpy keras-vis cython tqdm
+conda install -c anaconda mpi4py -y
+
+echo "********** INSTALLING TENSORFLOW GPU **********"
+pip install tensorflow
+
+echo "********** INSTALLING KERAS **********"
+pip install keras
+
+echo "********** TESTING TENSORFLOW **********"
+python -c "import tensorflow"
+
+echo "********** TESTING KERAS **********"
+python -c "import keras"
+
+echo ""
+echo "DONE!"
